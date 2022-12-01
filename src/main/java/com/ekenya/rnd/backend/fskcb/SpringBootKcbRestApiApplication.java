e8f24806870efd62d0f27ecd0a2ca5b888033c5f
David Charo <makuba@eclectics.io>
2022-12-01 11:40:30 +0300

    fix db issue

--- src/main/java/com/ekenya/rnd/backend/fskcb/SpringBootKcbRestApiApplication.java
+++ src/main/java/com/ekenya/rnd/backend/fskcb/SpringBootKcbRestApiApplication.java
@@ -19,6 +19,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
@@ -259,6 +260,11 @@ public class SpringBootKcbRestApiApplication   {
 		root.setAdditive(false); /* set to true if root should log too */
 
 		StatusPrinter.print(context);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
