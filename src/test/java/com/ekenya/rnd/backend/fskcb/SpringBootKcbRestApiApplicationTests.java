cd7433089a05565a94d290e3d5fa2dd2e444abd5
David Charo <makuba@eclectics.io>
2023-03-13 14:58:17 +0300

    add test folder

---
+++ src/test/java/com/ekenya/rnd/backend/fskcb/SpringBootKcbRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekenya.rnd.backend.fskcb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKcbRestApiApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
